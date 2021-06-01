b65e8c86709326b7298e6d48d4896bc2533924da
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-31 23:48:54 -0300

    Update index.jsx

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,9 +1,25 @@
 import '../Filme/FilmeInfo.css'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import api from '../../services/api'
 
-function Filme (){
-  return(
-    <div>
-      <h1>Página detalhes</h1>
+function Filme() {
+  const { id } = useParams()
+  const [filme, setFilme] = useState([])
+
+  useEffect(() => {
+    async function getFilme() {
+      const { data } = await api.get(`r-api/?api=filmes/${id}`)
+      setFilme(data);
+    }
+    getFilme()
+  }, [])
+
+  return (
+    <div key={filme.id}>
+      <h1>{filme.nome}</h1>
+      <img src={filme.foto} alt={filme.nome} />
+      <p>{filme.sinopse}</p>
     </div>
   )
 }
