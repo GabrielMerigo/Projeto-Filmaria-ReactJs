83e34ea9ae21a81efde6e5932150a0bd7c585320
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-01 00:30:55 -0300

    Update index.jsx

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,25 +1,50 @@
 import '../Filme/FilmeInfo.css'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import api from '../../services/api'
 
 function Filme() {
   const { id } = useParams()
   const [filme, setFilme] = useState([])
+  const [loading, setLoading] = useState(true)
+  const history = useHistory()
 
   useEffect(() => {
     async function getFilme() {
       const { data } = await api.get(`r-api/?api=filmes/${id}`)
-      setFilme(data);
-    }
-    getFilme()
-  }, [])
 
+      if (data.length === 0) {
+        //Tentou acessar uma página que não existe, retornei para a home.
+        history.replace('/')
+        return;
+      }
+
+      setFilme(data)
+      setLoading(false)
+    }
+
+    getFilme()
+    return () => {
+      console.log('Componente Desmontado');
+    }
+  }, [history, id])
+
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    )
+  }
   return (
     <div key={filme.id}>
       <h1>{filme.nome}</h1>
       <img src={filme.foto} alt={filme.nome} />
+      <strong>Sinopse</strong>
       <p>{filme.sinopse}</p>
+      <button>Salvar</button>
+      <button>Trailer</button>
     </div>
   )
 }
