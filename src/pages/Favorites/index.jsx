35346e87e00d482b52167e5eed92965056ba3c7d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-02 01:31:45 -0300

    Adicionando Funcionalidade de exclusão de props.

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import '../Favorites/Favoritos.css'
 
+function Favoritos() {
+  const filmes = JSON.parse(localStorage.getItem('filmes'))
 
-function Favoritos(){
-  return(
+  return (
     <div>
-      <h1>Filmes Favoritos</h1>
+      <h1 className="tituloFavorito">Meus Filmes</h1>
+      <div className="filmesDetails">
+        <ul>
+          {filmes.map(item => {
+            return (
+              <div key={item.id} className="filme">
+                <h4>{item.nome}</h4>
+                <div className="complements">
+                  <Link href="">Ver Detalhes</Link>
+                  <button>Excluir</button>
+                </div>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
