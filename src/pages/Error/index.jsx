937c460cbefe5380c903f40c83e940d81f782ec4
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-02 02:51:49 -0300

    Projeto Concluído, falta usar styled components.

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+import '../Error/Error.css'
+
 function Error(){
   return(
     <>
-      <h1>Página não encontrada...</h1>
+      <h1 className="titulo-erro">Página não encontrada...</h1>
+      <Link to="/">Veja todos os filmes.</Link>
     </>
   )
 }
