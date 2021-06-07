f5311ce915ea5c816dc08aaacfc7b7373a089b59
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-07 20:41:58 -0300

    Subindo para prd.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,7 +20,7 @@ function Home() {
         const { foto, nome, id } = filme
         return (
           <Container key={id}>
-            <Titulo tamanho={12} cor="ccc">{nome}</Titulo>
+            <Titulo tamanho={3} cor="zzz">{nome}</Titulo>
             <Img className="img" src={foto} alt="foto" />
             <Link to={`/filme/${id}`} className="acessar">Acessar</Link>
           </Container>
